0ab2dc392de37ed6e2d31572aca0716ef709d376
Dhayllin Jesus <dhayllin@hotmail.com>
2025-04-19 22:24:54 -0300

    provisionando máquinas para lab ansible

--- main.tf
+++ main.tf
@@ -3,30 +3,26 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-resource "digitalocean_droplet" "web" {
-  name   = "meu-primeiro-droplet"
-  region = "nyc3"        # pode ser sfo3, lon1, etc
-  size   = "s-1vcpu-1gb" # plano mais barato
-  image  = "ubuntu-22-04-x64"
+locals {
+  droplet_configs = [
+    {
+      name  = "ansiblelab-1"
+      image = "ubuntu-22-04-x64"
+    },
+    {
+      name  = "ansiblelab-2"
+      image = "centos-stream-9-x64"
+    }
+  ]
+}
 
+resource "digitalocean_droplet" "ansiblelab" {
+  count  = length(local.droplet_configs)
+  name   = local.droplet_configs[count.index].name
+  region = "nyc3"
+  size   = "s-1vcpu-1gb"
+  image  = local.droplet_configs[count.index].image
   ssh_keys = [var.ssh_key_fingerprint]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update -y
-              apt-get install -y ca-certificates curl gnupg lsb-release
-              install -m 0755 -d /etc/apt/keyrings
-              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-              chmod a+r /etc/apt/keyrings/docker.gpg
-              ARCH=$(dpkg --print-architecture)
-              RELEASE=$(lsb_release -cs)
-              echo "deb [arch=$${ARCH} signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $${RELEASE} stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-              apt-get update -y
-              apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-              systemctl start docker
-              systemctl enable docker
-              EOF
-
 }
 
 terraform {
@@ -39,8 +35,7 @@ terraform {
 }
 
 resource "digitalocean_project_resources" "attach" {
-  project = var.do_project_id
-  resources = [
-    digitalocean_droplet.web.urn
-  ]
+  count     = length(digitalocean_droplet.ansiblelab)
+  project   = var.do_project_id
+  resources = [digitalocean_droplet.ansiblelab[count.index].urn]
 }
